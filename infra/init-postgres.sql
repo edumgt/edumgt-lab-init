30edefc5ab97ada730bb9e62c1aa69b68be15a3b
kimdypm <kimdypm@gmail.com>
2026-07-03 12:30:40 +0900

    feat: add Keycloak service and database initialization to Docker setup

--- infra/init-postgres.sql
+++ infra/init-postgres.sql
@@ -11,3 +11,16 @@ GRANT ALL PRIVILEGES ON DATABASE dev TO postgres;
 CREATE USER raguser WITH PASSWORD 'ragpass';
 CREATE DATABASE ragdb OWNER raguser;
 GRANT ALL PRIVILEGES ON DATABASE ragdb TO raguser;
+
+-- keycloak-lab
+CREATE USER keycloak WITH PASSWORD 'keycloak';
+CREATE DATABASE keycloak OWNER keycloak;
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+
+CREATE USER app_user WITH PASSWORD 'app_pass';
+CREATE DATABASE app_db OWNER app_user;
+GRANT ALL PRIVILEGES ON DATABASE app_db TO app_user;
+
+CREATE USER airflow WITH PASSWORD 'airflow';
+CREATE DATABASE airflow OWNER airflow;
+GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
